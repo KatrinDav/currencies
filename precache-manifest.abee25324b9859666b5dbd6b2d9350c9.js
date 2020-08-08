42c771b26150085ebd20afe8b62145ede899b440
Katrin Dav <k.dawidowicz2016@gmail.com>
2020-08-08 19:13:20 +0200

    Updates

---
+++ precache-manifest.abee25324b9859666b5dbd6b2d9350c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "664d248b071e72f7a97637ac71530bd8",
+    "url": "/currencies/index.html"
+  },
+  {
+    "revision": "9d37b1e4971f7ca9adc5",
+    "url": "/currencies/static/css/3.c4106980.chunk.css"
+  },
+  {
+    "revision": "082ae1f968892ee40ad1",
+    "url": "/currencies/static/css/main.b9f5b20e.chunk.css"
+  },
+  {
+    "revision": "194c820c19480d802aa6",
+    "url": "/currencies/static/js/2.074f81e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/currencies/static/js/2.074f81e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d37b1e4971f7ca9adc5",
+    "url": "/currencies/static/js/3.6d348e43.chunk.js"
+  },
+  {
+    "revision": "082ae1f968892ee40ad1",
+    "url": "/currencies/static/js/main.b55df567.chunk.js"
+  },
+  {
+    "revision": "b1765fe066f6201afb86",
+    "url": "/currencies/static/js/runtime-main.4bdcf93d.js"
+  }
+]);
